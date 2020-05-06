a7188ce4df247d53ef223ebe3744d1386068b9b8
yuanhawk <yuanmirage@yahoo.com>
2020-05-06 22:47:58 +0800

    Initial commit

---
+++ Python Project/Wk 6 In Class Activities + Homework/Week 6.py
@@ -0,0 +1,21 @@
+
+Dictionary
+
+#Empty dictionary
+d = {} or d = dict()
+
+#Add
+d[key] = value
+
+#Remove
+del d[key] || d.pop[key]
+
+#Visit key, value, items
+d.keys() || d.items() || d.values()
+
+#Traverse all keys, value, items
+
+for x in d.keys() || d.values()
+
+x in d => reference to keys
+d = {'a': 1, 'b': 2}
